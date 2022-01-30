cfb736c37ba06cd068e2f9cfe190d5454ab8fcc9
Daniel Carstensen <daniel.l.carstensen.24@dartmouth.edu>
2022-01-30 14:29:14 -0500

    Commented out the out of bounds message since it's not working

--- PS2/PointQuadtree.java
+++ PS2/PointQuadtree.java
@@ -70,9 +70,9 @@ public class PointQuadtree<E extends Point2D> {
 	 * Inserts the point into the tree
 	 */
 	public void insert(E p2) { // uses the mathematical quadrants
-		if (p2.getX() < this.x1 || p2.getX() > this.x2 || p2.getY() < this.y1 || p2.getY() > this.y2) {
-			System.out.println("coordinate out of bounds");
-		} else if (p2.getX() <= point.getX()) {
+//		if (p2.getX() < this.x1 || p2.getX() > this.x2 || p2.getY() < this.y1 || p2.getY() > this.y2) {
+//			System.out.println("coordinate out of bounds");
+		if (p2.getX() <= point.getX()) {
 			if (p2.getY() <= point.getY()) {
 				if (hasChild(2)) {
 					c2.insert(p2);

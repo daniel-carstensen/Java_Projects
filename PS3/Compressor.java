706830ade409f51201a02bfe24f896585c5ba30d
Max Lawrence <william.m.lawrence.24@dartmouth.edu>
2022-02-09 16:45:10 -0500

    detailed comments, new Compressor constructor, new test in main(), and new files to hold the new test output

--- PS3/Compressor.java
+++ PS3/Compressor.java
@@ -13,8 +13,20 @@ public class Compressor {
      */
     public Compressor(String filePath) {
         this.filePath = filePath;
-        this.compressedFilePath = "PS3/compressedFile.txt";
-        this.decompressedFilePath = "PS3/decompressedFile.txt";
+        this.compressedFilePath = "PS3/compressedFile.txt"; // default file name for testing
+        this.decompressedFilePath = "PS3/decompressedFile.txt"; // default file name for testing
+    }
+
+    /**
+     * Constructor
+     * @param filePath the path name of the file to open
+     * @param compressedFilePath the path name of the file to write the compressed file into
+     * @param decompressedFilePath the path name of the file to write the decompressed file into
+     */
+    public Compressor(String filePath, String compressedFilePath, String decompressedFilePath) {
+        this.filePath = filePath;
+        this.compressedFilePath = compressedFilePath;
+        this.decompressedFilePath = decompressedFilePath;
     }
 
     /**
@@ -24,19 +36,19 @@ public class Compressor {
      * @throws IOException
      */
     private HashMap<Character, Integer> fillTable () throws IOException {
-        BufferedReader input = new BufferedReader(new FileReader(filePath));
+        BufferedReader input = new BufferedReader(new FileReader(filePath)); // creating a reader
+        // create a HashMap to hold characters and frequencies
         HashMap<Character, Integer> frequencyMap = new HashMap<>();
-        
-        while (input.ready()) {
+        while (input.ready()) { // loop through all characters in the input file
             char currentC = (char) input.read();
-            if (frequencyMap.containsKey(currentC)) {
-                int newFrequency = frequencyMap.get(currentC) + 1;
-                frequencyMap.put(currentC, newFrequency);
+            if (frequencyMap.containsKey(currentC)) { // if this character has already been added ...
+                int newFrequency = frequencyMap.get(currentC) + 1; // increment the frequency
+                frequencyMap.put(currentC, newFrequency); // and override it with the newFrequency
             }
-            else frequencyMap.put(currentC, 1);
+            else frequencyMap.put(currentC, 1); // if the character was not already added, add it with frequency 1
         }
         input.close();
-        return frequencyMap;
+        return frequencyMap; // return the final HashMap
     }
 
     /**
@@ -45,16 +57,19 @@ public class Compressor {
      * @return initialTreeQueue a PriorityQueue which holds BinaryTree nodes that hold CharFreq objects
      * @throws IOException
      */
-    private PriorityQueue fillQueue () throws IOException { // im confused here.  Where do b1 and b2 come from?
+    private PriorityQueue fillQueue () throws IOException {
+        // Using an anonymous comparator
         PriorityQueue<BinaryTree<CharFreq>> initialTreeQueue = new PriorityQueue<>((b1, b2) -> {
             if (b1.getData().getFrequency() < b2.getData().getFrequency()) return -1;
             else if (b1.getData().getFrequency() == b2.getData().getFrequency()) return 0;
             else return 1;
         });
-        HashMap<Character, Integer> frequencyMap = fillTable();
 
-        Set<Character> allCharacters = frequencyMap.keySet();
+        HashMap<Character, Integer> frequencyMap = fillTable(); // get the character and frequency map using fillTable()
+
+        Set<Character> allCharacters = frequencyMap.keySet(); // creating a set from the map to index through
         for (Character key : allCharacters) {
+            // add each CharFreq object to the initialTreeQueue with the comparator organizing them by frequency
             initialTreeQueue.add(new BinaryTree<>(new CharFreq(key, frequencyMap.get(key))));
         }
         return initialTreeQueue;
@@ -66,8 +81,18 @@ public class Compressor {
      * @throws IOException
      */
     private BinaryTree createTree () throws IOException {
-        PriorityQueue<BinaryTree<CharFreq>> initialTreeQueue = fillQueue();
-        return recursiveBuilding(initialTreeQueue);
+        PriorityQueue<BinaryTree<CharFreq>> initialTreeQueue = fillQueue(); // get the Queue by calling fillQueue()
+        // if there was only one character type (either repeater or one instance) in the original file,
+        // construct the binary tree by simply adding this CharFreq object to a root and returning the tree
+        if (initialTreeQueue.size() == 1) {
+            BinaryTree<CharFreq> t1 = initialTreeQueue.poll();
+            CharFreq root = new CharFreq('/', t1.getData().getFrequency());
+            return new BinaryTree<CharFreq>(root, t1, null);
+        }
+        else if (initialTreeQueue.isEmpty()) { // if the original file was empty, return an empty tree
+            return new BinaryTree<CharFreq>(null);
+        }
+        return recursiveBuilding(initialTreeQueue); // otherwise, call recursiveBuilding() to construct the tree
     }
 
     /**
@@ -78,14 +103,17 @@ public class Compressor {
      * @return a BinaryTree
      */
     private BinaryTree recursiveBuilding(PriorityQueue<BinaryTree<CharFreq>> initialTreeQueue) {
-        if (initialTreeQueue.size() >= 2) {
+        if (initialTreeQueue.size() >= 2) { // if there is not yet only 1 binary tree held in the queue
+            // pop off and store the first two BinaryTrees in the queue
             BinaryTree<CharFreq> t1 = initialTreeQueue.poll();
             BinaryTree<CharFreq> t2 = initialTreeQueue.poll();
+            // create a root with frequency value equal to the sum of t1.frequency and t2.frequency and an arbitrary
+            // character value
             CharFreq root = new CharFreq('/', t1.getData().getFrequency() + t2.getData().getFrequency());
-            initialTreeQueue.add(new BinaryTree<CharFreq>(root, t1, t2));
-             return recursiveBuilding(initialTreeQueue);
+            initialTreeQueue.add(new BinaryTree<CharFreq>(root, t1, t2)); // add the root back into the Queue
+             return recursiveBuilding(initialTreeQueue); // recursively call the method with the new initialTreeQueue
         }
-        return initialTreeQueue.poll();
+        return initialTreeQueue.poll(); // remove and return the final tree from initialTreeQueue
     }
 
     /**
@@ -94,8 +122,11 @@ public class Compressor {
      * @return codeMap a HashMap holding characters and their paths from the BinaryTree
      */
     public HashMap<Character, String> getCodeMap(BinaryTree<CharFreq> tree) {
-        HashMap<Character, String> codeMap = new HashMap<Character, String>();
-        return codeMapRecursion(tree, "", codeMap);
+        HashMap<Character, String> codeMap = new HashMap<Character, String>(); // create empty HashMap, codeMap
+        if (tree.getData() == null) { // if the tree is empty, return an empty codeMap
+            return codeMap;
+        }
+        return codeMapRecursion(tree, "", codeMap); // otherwise, call codeMapRecursion() to fill the codeMap
 
     }
 
@@ -108,6 +139,7 @@ public class Compressor {
      * @return
      */
     private HashMap<Character, String> codeMapRecursion(BinaryTree<CharFreq> tree, String path, HashMap<Character, String> codeMap) {
+        // recursively traversing the tree
         if (tree.hasLeft()) {
             codeMapRecursion(tree.getLeft(), path + "0", codeMap);
         }
@@ -115,6 +147,8 @@ public class Compressor {
             codeMapRecursion(tree.getRight(), path + "1", codeMap);
         }
         if (tree.isLeaf()) {
+            // each leaf is a real CharFreq node, so when a leaf is reached return its character and the path to
+            // get to it
             codeMap.put(tree.getData().getCharacter(), path);
         }
         return codeMap;
@@ -126,15 +160,16 @@ public class Compressor {
      * @throws IOException
      */
     private void compress() throws IOException {
-        BufferedReader input = new BufferedReader(new FileReader(filePath));
-        BufferedBitWriter bitOutput = new BufferedBitWriter(compressedFilePath);
-        BinaryTree<CharFreq> tree = createTree();
-        HashMap<Character, String> codeMap = getCodeMap(tree);
-        while (input.ready()) {
+        BufferedReader input = new BufferedReader(new FileReader(filePath)); // create text reader
+        BufferedBitWriter bitOutput = new BufferedBitWriter(compressedFilePath); // create bit writer
+        BinaryTree<CharFreq> tree = createTree(); // create the tree using createTree()
+        HashMap<Character, String> codeMap = getCodeMap(tree); // create the codeMap by calling getCodeMap() on the tree
+        while (input.ready()) { // for each character in the file
             char currentC = (char) input.read();
-            String code = codeMap.get(currentC);
-            char[] codeArray = code.toCharArray();
+            String code = codeMap.get(currentC); // get the path or "code" for the character from the codeMap
+            char[] codeArray = code.toCharArray(); // convert the code to an Array so that it can be easily looped through
             for (char c : codeArray) {
+                // write out each character in codeArray as bits (false for '0' and true for '1') into the new file
                 if (c == '0') {
                     bitOutput.writeBit(false);
                 }
@@ -153,11 +188,12 @@ public class Compressor {
      * @throws IOException
      */
     private void decompress() throws IOException {
-        BufferedBitReader bitInput = new BufferedBitReader(compressedFilePath);
-        BufferedWriter output = new BufferedWriter(new FileWriter(decompressedFilePath));
-        BinaryTree<CharFreq> originaltree = createTree();
+        BufferedBitReader bitInput = new BufferedBitReader(compressedFilePath); // create a bit reader
+        BufferedWriter output = new BufferedWriter(new FileWriter(decompressedFilePath)); // create a text writer
+        BinaryTree<CharFreq> originaltree = createTree(); // creates the tree to use as a code for decompression
         BinaryTree<CharFreq> tree = originaltree;
         while (bitInput.hasNext()) {
+            // for each bit, progress through the tree
             boolean bit = bitInput.readBit();
             if (bit) {
                 tree = tree.getRight();
@@ -165,9 +201,9 @@ public class Compressor {
             if (!bit) {
                 tree = tree.getLeft();
             }
-            if (tree.isLeaf()) {
+            if (tree.isLeaf()) { // when you hit a leaf, write it's character into the decompressed file
                 output.write(tree.getData().getCharacter());
-                tree = originaltree;
+                tree = originaltree; // reset the tree for the next character
             }
         }
         bitInput.close();
@@ -175,12 +211,11 @@ public class Compressor {
     }
 
     public static void main(String[] args) throws IOException {
-        Compressor usConstitution = new Compressor("PS3/WarAndPeace.txt");
-//        Object[] a = usConstitution.fillQueue().toArray();
-//        for (int j = 0; j < a.length; j++)
-//            System.out.println(a[j]);
-//        System.out.println(usConstitution.getCodeMap(usConstitution.createTree()));
+        Compressor usConstitution = new Compressor("PS3/USConstitution.txt",
+                "PS3/compressedUSConstitution.txt",
+                "PS3/decompressedUSConstitution.txt");
         usConstitution.compress();
         usConstitution.decompress();
+
     }
 }

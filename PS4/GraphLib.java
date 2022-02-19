cd4e705ec857f92f29800472beeed3bba72494a6
Daniel Carstensen <daniel.l.carstensen.24@dartmouth.edu>
2022-02-19 15:00:41 -0500

    commented GraphLib

--- PS4/GraphLib.java
+++ PS4/GraphLib.java
@@ -1,5 +1,3 @@
-import net.datastructures.Tree;
-
 import java.util.*;
 
 /**
@@ -11,119 +9,101 @@ import java.util.*;
  */
 public class GraphLib {
 	/**
-	 * Takes a random walk from a vertex, up to a given number of steps
-	 * So a 0-step path only includes start, while a 1-step path includes start and one of its out-neighbors,
-	 * and a 2-step path includes start, an out-neighbor, and one of the out-neighbor's out-neighbors
-	 * Stops earlier if no step can be taken (i.e., reach a vertex with no out-edge)
+	 * Performs a bfs on the graph from a specified start vertex and builds a tree graph with the starting vertex
+	 * at the root
 	 *
-	 * @param g     graph to walk on
-	 * @param start initial vertex (assumed to be in graph)
-	 * @param steps max number of steps
-	 * @return a list of vertices starting with start, each with an edge to the sequentially next in the list;
-	 * null if start isn't in graph
-	 */
-	public static <V, E> List<V> randomWalk(Graph<V, E> g, V start, int steps) {
-		if (!g.hasVertex(start)) return null;
-
-		int remainingSteps = steps;
-		V currentVertex = start;
-		List<V> vertexList = new ArrayList<V>();
-
-		vertexList.add(start);
-
-		while (remainingSteps > 0) {
-			if (g.outDegree(currentVertex) == 0) return vertexList;
-			else {
-				List<V> outNeighborList = new ArrayList<>();
-				Iterable<V> outNeighbors = g.outNeighbors(currentVertex);
-				for (V vertex : outNeighbors) {
-					outNeighborList.add(vertex);
-				}
-				int direction = (int) (Math.random() * (outNeighborList.size()));
-				currentVertex = outNeighborList.get(direction);
-				vertexList.add(currentVertex);
-				remainingSteps--;
-			}
-		}
-
-		return vertexList;
-	}
-
-	/**
-	 * Orders vertices in decreasing order by their in-degree
+	 * @param graph graph
+	 * @param source starting vertex
 	 *
-	 * @param g graph
-	 * @return list of vertices sorted by in-degree, decreasing (i.e., largest at index 0)
+	 * @return backTrack tree graph with the shortest paths from any connected vertex back to the starting vertex
 	 */
-	public static <V, E> List<V> verticesByInDegree(Graph<V, E> g) {
-		List<V> allVertices = new ArrayList<>();
-		Iterable<V> vertices = g.vertices();
-		for (V vertex : vertices) {
-			allVertices.add(vertex);
-		}
+	public static <V, E> Graph<V, E> bfs(Graph<V, E> graph, V source) {
+		Graph<V, E> backTrack = new AdjacencyMapGraph<>(); // initialize backTrack tree graph
+		backTrack.insertVertex(source); // load start vertex into graph
+		Set<V> visited = new HashSet<V>(); // Set to track which vertices have already been visited
+		Queue<V> queue = new LinkedList<V>(); // queue to implement BFS
 
-		allVertices.sort((v1, v2) -> (
-				g.inDegree(v2) - g.inDegree(v1)
-				)
-		);
-
-		return allVertices;
-	}
-
-	public static <V, E> Graph<V, E> bfs(Graph<V, E> g, V source) {
-		Graph<V, E> backTrack = new AdjacencyMapGraph<>(); //initialize backTrack
-		backTrack.insertVertex(source); //load start vertex with null parent
-		Set<V> visited = new HashSet<V>(); //Set to track which vertices have already been visited
-		Queue<V> queue = new LinkedList<V>(); //queue to implement BFS
-
-		queue.add(source); //enqueue start vertex
-		visited.add(source); //add start to visited Set
-		while (!queue.isEmpty()) { //loop until no more vertices
-			V u = queue.remove(); //dequeue
-			for (V v : g.outNeighbors(u)) { //loop over out neighbors
-				if (!visited.contains(v)) { //if neighbor not visited, then neighbor is discovered from this vertex
-					visited.add(v); //add neighbor to visited Set
-					queue.add(v); //enqueue neighbor
-					backTrack.insertVertex(v);
-					backTrack.insertDirected(v, u, g.getLabel(v, u)); //save that this vertex was discovered from prior vertex
+		queue.add(source); // enqueue start vertex
+		visited.add(source); // add start to visited Set
+		while (!queue.isEmpty()) { // loop until no more vertices
+			V u = queue.remove(); // dequeue
+			for (V v : graph.outNeighbors(u)) { // loop over out neighbors
+				if (!visited.contains(v)) { // if neighbor not visited, then neighbor is discovered from this vertex
+					visited.add(v); // add neighbor to visited Set
+					queue.add(v); // enqueue neighbor
+					backTrack.insertVertex(v); // insert neighbor into backTrack graph
+					backTrack.insertDirected(v, u, graph.getLabel(v, u)); // save that this vertex was discovered from prior vertex
 				}
 			}
 		}
 		return backTrack;
 	}
 
+	/**
+	 * Find the path from a vertex to the vertex at the root in the tree graph created in BFS and return it
+	 * as a list
+	 *
+	 * @param tree tree graph
+	 * @param v input vertex in graph
+	 *
+	 * @return list that specifies the path from v to the root
+	 */
 	public static <V, E> List<V> getPath(Graph<V, E> tree, V v) {
-		List<V> path = new ArrayList<>();
-		path.add(v);
-		Iterator<V> out = tree.outNeighbors(v).iterator();
-		while (out.hasNext()) {
-			v = out.next();
-			out = tree.outNeighbors(v).iterator();
-			path.add(v);
+		List<V> path = new ArrayList<>(); // initialize path list
+		path.add(v); // add input vertex to list
+		Iterator<V> out = tree.outNeighbors(v).iterator(); // initialize iterator to iterate through outNeighbors of input vertex
+		while (out.hasNext()) {	// while the vertex as an outNeighbor
+			v = out.next();	// redefine vertex as its outNeighbor
+			out = tree.outNeighbors(v).iterator();	// redefine iterator
+			path.add(v); // add vertex to the path list
 		}
 		return path;
 	}
 
-	public static <V, E> Set<V> missingVertices(Graph<V, E> graph, Graph<V, E> subgraph) {
-		Set<V> missingVertices = new HashSet<>();
 
-		for (V v : graph.vertices()) {
-			if (!subgraph.hasVertex(v)) missingVertices.add(v);
+	/**
+	 * find all vertices in graph that are not present in a subgraph (in our case created by BFS) and insert them to a Set
+	 *
+	 * @param graph graph
+	 * @param subgraph subgraph
+
+	 * @return
+	 */
+	public static <V, E> Set<V> missingVertices(Graph<V, E> graph, Graph<V, E> subgraph) {
+		Set<V> missingVertices = new HashSet<>(); // initialize a Set of missingVertices
+
+		for (V v : graph.vertices()) { // for all vertices in graph
+			if (!subgraph.hasVertex(v)) missingVertices.add(v); // if subgraph does not contain vertex add to Set
 		}
 		return missingVertices;
 	}
 
-	public static <V, E> double averageSeparation(Graph<V, E> tree, V root) {
-		int total;
-		total = totalDistance(tree, root, 0);
+	/**
+	 * calculate the average distance from a root to every other vertex in a tree graph
+	 *
+	 * @param tree tree
+	 * @param root root
 
-		return (double) total / (double) tree.numVertices();
+	 * @return average separation as a double
+	 */
+	public static <V, E> double averageSeparation(Graph<V, E> tree, V root) {
+		int total; // create a total integer variable
+		total = totalDistance(tree, root, 0); // set total equal to return value of recursive function
+		return (double) total / (double) tree.numVertices(); // return total divided by total number of vertices in graph
 	}
 
+	/**
+	 *
+	 * @param tree tree
+	 * @param currentV current vertex
+	 * @param currentLevel current level in the tree, i.e. distance from the root
+
+	 * @return total distance of all vertices from the root
+	 */
 	public static  <V,E> int totalDistance(Graph<V,E> tree, V currentV, int currentLevel) {
-		int total = currentLevel;
-		for (V v : tree.inNeighbors(currentV)) {
-			total += totalDistance(tree, v, currentLevel+1);
+		int total = currentLevel; // set total equal to current level
+		for (V v : tree.inNeighbors(currentV)) { // for all vertices that point to the current vertex
+			total += totalDistance(tree, v, currentLevel+1); // recursively call totalDistance and add return value to total distance
 		}
 		return total;
 	}

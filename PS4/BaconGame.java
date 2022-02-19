b7c01a89a053c1a005b1a24eb52a296ea538dcdd
Daniel Carstensen <daniel.l.carstensen.24@dartmouth.edu>
2022-02-19 17:17:36 -0500

    Stuff

--- PS4/BaconGame.java
+++ PS4/BaconGame.java
@@ -1,4 +1,7 @@
+import net.datastructures.Vertex;
+
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.*;
 
 public class BaconGame {
@@ -6,8 +9,27 @@ public class BaconGame {
     private AdjacencyMapGraph<String, Set<String>> graph;
     private Graph<String, Set<String>> tree;
 
+
+    private class Actor {
+        String actor;
+        double value;
+
+        public Actor(String actor, double value) {
+            this.actor = actor;
+            this.value = value;
+        }
+
+        public String getActor() {
+            return actor;
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
+
     public BaconGame(String center, String actorsFile, String moviesFile, String actorsMoviesFile) throws IOException {
-        GraphBuilder graphBuilder =  new GraphBuilder(actorsFile, moviesFile, actorsMoviesFile);
+        GraphBuilder graphBuilder = new GraphBuilder(actorsFile, moviesFile, actorsMoviesFile);
         this.graph = graphBuilder.buildGraph();
         this.center = center;
     }
@@ -23,12 +45,14 @@ public class BaconGame {
         System.out.println("q: quit game");
         newCenter("Kevin Bacon");
     }
+
     public void newCenter(String center) {
         this.center = center;
         this.tree = GraphLib.bfs(graph, this.center);
         System.out.println(this.center + " is now the center of the acting universe, connected to " + tree.numVertices()
                 + "/" + graph.numVertices() + " actors with average separation " + GraphLib.averageSeparation(tree, this.center));
     }
+
     public void readInput() {
         Scanner input = new Scanner(System.in);
         String userInput = input.next();
@@ -47,6 +71,7 @@ public class BaconGame {
             System.out.println(missingActor);
         }
     }
+
     public void printPath(String actor) {
         List<String> path = GraphLib.getPath(tree, actor);
         for (int indx = 0; indx < path.size() - 1; indx++) {
@@ -58,6 +83,31 @@ public class BaconGame {
         }
     }
 
+    public void bestCenters(int num) {
+        Graph<String, Set<String>> baconUniverse = GraphLib.bfs(this.graph, "Kevin Bacon");
+        Set<String> loners = GraphLib.missingVertices(this.graph, baconUniverse);
+        PriorityQueue<Actor> centerQueue;
+        if (num < 0) {
+            centerQueue = new PriorityQueue<Actor>((a1, a2) -> (Double.compare(a1.getValue(), a2.getValue())));
+        } else {
+            centerQueue = new PriorityQueue<Actor>((a1, a2) -> Double.compare(a2.getValue(), a1.getValue()));
+        }
+
+        for (String v : baconUniverse.vertices()) {
+            Graph<String, Set<String>> tree = GraphLib.bfs(this.graph, v);
+            double averageSeparation = GraphLib.averageSeparation(tree, v);
+            BaconGame.Actor centerSeparation = new BaconGame.Actor(v, averageSeparation);
+            centerQueue.add(centerSeparation);
+        }
+
+        for (int i = 1; i <= Math.abs(num); ++i) {
+            BaconGame.Actor actor = centerQueue.poll();
+            System.out.println(actor.getActor() + " with average separation " + actor.getValue());
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        BaconGame test = new BaconGame("Kevin Bacon", "PS4/actors.txt", "PS4/movies.txt", "PS4/movie-actors.txt");
+        test.bestCenters(-10);
     }
 }

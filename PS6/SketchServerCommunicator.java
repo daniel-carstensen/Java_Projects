adf2151b575e31d288e519b1c6d7a593f205fe99
Max Lawrence <william.m.lawrence.24@dartmouth.edu>
2022-03-06 15:07:03 -0500

    updated sketchServerCommunicator

--- PS6/SketchServerCommunicator.java
+++ PS6/SketchServerCommunicator.java
@@ -26,6 +26,11 @@ public class SketchServerCommunicator extends Thread {
 	public void send(String msg) {
 		out.println(msg);
 	}
+
+	public synchronized void queueChange(String msg) {
+		SLLQueue<String> queue = server.getChangeRequests();
+		queue.enqueue(msg);
+	}
 	
 	/**
 	 * Keeps listening for and handling (your code) messages from the client
@@ -43,53 +48,11 @@ public class SketchServerCommunicator extends Thread {
 
 			// Keep getting and handling messages from the client
 			String line;
-			Sketch sketch = server.getSketch();
-			int id;
 			while ((line = in.readLine()) != null) {
 				System.out.println("message received");
 				System.out.println(line);
-				String[] pieces = line.split(" ");
-				if (pieces[0].equals("ellipse")) {
-					id = sketch.addShape(new Ellipse(Integer.parseInt(pieces[1]), Integer.parseInt(pieces[2]),
-							Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), new Color(Integer.parseInt(pieces[5]))));
-					server.broadcast(id + " " + server.getSketch().getShape(id).toString());
-				}
-				else if (pieces[0].equals("rectangle")) {
-					id = sketch.addShape(new Rectangle(Integer.parseInt(pieces[1]), Integer.parseInt(pieces[2]),
-							Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), new Color(Integer.parseInt(pieces[5]))));
-					server.broadcast(id + " " + server.getSketch().getShape(id).toString());
-				}
-				else if (pieces[0].equals("segment")) {
-					id = sketch.addShape(new Segment(Integer.parseInt(pieces[1]), Integer.parseInt(pieces[2]),
-							Integer.parseInt(pieces[3]), Integer.parseInt(pieces[4]), new Color(Integer.parseInt(pieces[5]))));
-					server.broadcast(id + " " + server.getSketch().getShape(id).toString());
-				}
-				else if (pieces[0].equals("polyline")) {
-					ArrayList<Point> points = new ArrayList<>();
-					int i = 1;
-					while (!pieces[i + 2].equals(":")) {
-						points.add(new Point(Integer.parseInt(pieces[i]), Integer.parseInt(pieces[i + 1])));
-						i += 2;
-					}
-					id = sketch.addShape(new Polyline(points, new Color(Integer.parseInt(pieces[i + 3]))));
-					server.broadcast(id + " " + server.getSketch().getShape(id).toString());
-				}
-				else if (pieces[1].equals("move")) {
-					id = Integer.parseInt(pieces[0]);
-					sketch.getShape(id).moveBy(Integer.parseInt(pieces[2]), Integer.parseInt(pieces[3]));
-					server.broadcast(id + " " + "move" + " " + pieces[2] + " " + pieces[3]);
-				}
-				else if (pieces[1].equals("recolor")) {
-					id = Integer.parseInt(pieces[0]);
-					Color color = new Color(Integer.parseInt(pieces[2]));
-					sketch.getShape(id).setColor(color);
-					server.broadcast(id + " " + "recolor" + " " + color.getRGB());
-				}
-				else if (pieces[1].equals("remove")) {
-					id = Integer.parseInt(pieces[0]);
-					sketch.removeShape(id);
-					server.broadcast(id + " " + "remove");
-				}
+				queueChange(line);
+
 			}
 
 			// Clean up -- note that also remove self from server's list so it doesn't broadcast here
